41e2669f7e90b686b06d9fd7578258edfa95d7a0
egjallow10 <egjallow10@gmail.com>
2023-04-17 22:18:56 +0000

    changes

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,21 +28,12 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Returns paginated values
-
-        Args:
-            page (int, optional): _description_. Defaults to 1.
-            page_size (int, optional): _description_. Defaults to 10.
-
-        Returns:
-            List[List]: _description_
         """
         page_list: list = []
-
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         try:
