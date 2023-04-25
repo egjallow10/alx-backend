151c0c6ac788883f0f3e0aec863ad0549686610c
egjallow10 <egjallow10@gmail.com>
2023-04-25 09:34:51 -0700

    hello world

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+"""A python Flask program for hello world"""
 
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    """Render a static file 0-index.html"""
     return render_template("/templates/0-index.html")
