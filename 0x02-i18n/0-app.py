a3ac09e1d257c78c283ad2afea8fc3757b52c82c
egjallow10 <egjallow10@gmail.com>
2023-04-25 09:58:43 -0700

    hello world

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def hello():
     """Render a static file 0-index.html"""
-    return render_template("/templates/0-index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
