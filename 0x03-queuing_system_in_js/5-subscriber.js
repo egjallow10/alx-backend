8e61e2d4437bcc7ae40c9df3fa886ec9bbeb6be5
egjallo <egjallo@insistglobal.com>
2023-06-01 15:12:54 +0000

    edit my function

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,24 +1,33 @@
 const redis = require('redis')
 const client = redis.createClient()
 const publisher = redis.createClient()
-const subcriber = redis.createClient()
+// const subcriber = redis.createClient()
 const channel = 'holberton school channel'
 
 
 publisher.on('connect',erro => console.log(`Redis client connected to the server`))
 publisher.on('error',erro => console.log(`Redis client not connected to the server: ${erro}`))
 
-publisher.publish(channel, channel)
-subcriber.subscribe(channel)
+// subcriber.subscribe(channel, message);
 
-subcriber.on('message', (theChannel, message) => {
+// publisher.on('message', (theChannel, message) => {
+//     if (message === 'KILL_SERVER') {
+//         subcriber.unsubscribe();
+//         subcriber.quit();
+//         publishMessage
+//     }
+// })
+
+const publishMessage = (meaasge, time) => {
+    
     if (message === 'KILL_SERVER') {
         subcriber.unsubscribe();
         subcriber.quit();
+        
     }
-})
-
-const publishMessage = (meaasge, time) => {
-    console.log(`About to send ${meaasge}`)
-    publisher.publish(channel, channel)
+    setInterval(()=>{
+        console.log(`About to send ${meaasge}`)
+        publisher.publish(channel, meaasge)
+    },time)
+    
 }
