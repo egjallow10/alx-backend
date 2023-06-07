6587d519b7a5c0260f08313580c4f443958b4ef6
egjallow10 <egjallow10@gmail.com>
2023-06-07 21:04:18 +0000

    publish

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,33 +1,36 @@
 const redis = require('redis')
 const client = redis.createClient()
 const publisher = redis.createClient()
-// const subcriber = redis.createClient()
-const channel = 'holberton school channel'
+const subcriber = redis.createClient()
+const logMessage = 'holberton school channel'
 
 
 publisher.on('connect',erro => console.log(`Redis client connected to the server`))
 publisher.on('error',erro => console.log(`Redis client not connected to the server: ${erro}`))
 
-// subcriber.subscribe(channel, message);
+// publisher.publish(channel, channel)
+// subcriber.subscribe(channel)
 
-// publisher.on('message', (theChannel, message) => {
+// subcriber.on('message', (theChannel, message) => {
 //     if (message === 'KILL_SERVER') {
 //         subcriber.unsubscribe();
 //         subcriber.quit();
-//         publishMessage
 //     }
 // })
 
 const publishMessage = (meaasge, time) => {
-    
-    if (message === 'KILL_SERVER') {
-        subcriber.unsubscribe();
-        subcriber.quit();
-        
+    if(meaasge === 'KILL_SERVER'){
+        publisher.quit()
     }
-    setInterval(()=>{
-        console.log(`About to send ${meaasge}`)
-        publisher.publish(channel, meaasge)
-    },time)
     
+    setTimeout(() =>{
+        console.log(`About to send ${meaasge}`);
+        publisher.publish(logMessage, meaasge);
+
+
+    },time)
 }
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
