574dab99915fc2da0a99ab489f791e75dcecac2a
egjallow10 <egjallow10@gmail.com>
2023-09-29 11:37:20 +0000

    modify to get desire result

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,27 +1,32 @@
-import redis from 'redis'
-import { createClient } from "redis";
-const client = createClient()
+import redis from 'redis';
+import { createClient } from 'redis';
+const client = createClient();
 
-client.on('connect', err => console.log(`Redis client connected to the server`));
-client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+client.on('connect', (err) =>
+  console.log(`Redis client connected to the server`),
+);
+client.on('error', (err) =>
+  console.log(`Redis client not connected to the server: ${err}`),
+);
 
 // client.connected(() => console.log(`Redis client connected to the server`));
 // await client.set('key', 'value');
 // await client.disconnect();
 
 const setNewSchool = async (schoolName, value) => {
-  
-client.set(schoolName, value, redis.print)
-    
-}
+  client.set(schoolName, value, redis.print);
+};
 
 const displaySchoolValue = async (schoolName) => {
-    client.get(schoolName, redis.print);
-   
-}
+  client.get(`${schoolName}`, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(result);
+    }
+  });
+};
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
 displaySchoolValue('HolbertonSanFrancisco');
-
-
